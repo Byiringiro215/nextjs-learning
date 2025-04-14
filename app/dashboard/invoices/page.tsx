f5f9e57803e0316f2cfdeeeec5cc34685f172b73
byiringiro215 <emmabyiringiro215@gmail.com>
2025-04-14 10:57:35 +0200

    first push

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Invoices() {
+  return (
+    <div>
+      <h1>Invoices page</h1>
+    
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard() {
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+    
+    </div>
+  );
+}
